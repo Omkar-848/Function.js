4191779ae8db29a2439458c563e02ccc1679680d
OMKAR CHAVAN <ovchavan3456@gmail.com>
2025-01-28 06:33:20 -0800

    Add files via upload

---
+++ Function.js
@@ -0,0 +1,8 @@
+// Function in javascript are reusable blocks of code designed to perform specific tasks 
+// they allow you to organize,reuse,and modularize code. it can take inputs 
+// perform actions and return outputs
+
+function sum (x,y){
+    return x+y;
+}
+console.log(sum(6,9));
---
+++ Function1.js
@@ -0,0 +1,21 @@
+// example of function
+
+function arr(){
+    console.log("H")
+    console.log("I")
+    console.log("J")
+    console.log("A")
+    console.log("V")
+    console.log("A")
+    }
+    arr()
+
+        //example no 2
+        function addTwonumbers(num1,num2){
+        // let result=num1+num2
+        //return result
+        //console.log("sham")
+        return num1+num2
+    }
+    const result =addTwonumbers(2,6)
+    console.log("Result:" ,result);
